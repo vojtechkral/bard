72e9a8ed83151175d48ceaf628d66e3e283577bc
Vojtech Kral <vojtech@kral.hk>
2021-07-22 23:53:28 +0200

    Fix: Return non-zero status on error

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,13 @@
 use std::env;
+use std::process;
 
 use bard::{bard, cli};
 
 fn main() {
     let args: Vec<_> = env::args_os().collect();
     cli::use_stderr(true);
-    bard(&args[..]).unwrap_or_else(cli::error);
+    if let Err(err) = bard(&args[..]) {
+        cli::error(err);
+        process::exit(1);
+    }
 }
