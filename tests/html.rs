4e98d3b2c5ad11d5a5f964f0a688bb2d2da81cf6
Vojtech Kral <vojtech@kral.pm>
2023-03-22 00:22:32 +0100

    Testcase for reserved HTML tags, refactor html tests using utils_ng

--- tests/html.rs
+++ tests/html.rs
@@ -1,27 +1,77 @@
-use std::fs;
+use bard::{
+    parser::{html::RESERVED_TAGS, DiagKind},
+    render::{html, pdf},
+};
 
-use regex::Regex;
-
-mod util;
-pub use util::*;
+mod util_ng;
+pub use util_ng::*;
 
 #[test]
 fn project_html() {
-    let build = Builder::build(TEST_PROJECTS / "html").unwrap();
-    let out_dir = build.project.settings.dir_output();
+    #[rustfmt::skip]
+    let build = TestProject::new("html")
+        .output("songbook.pdf")
+        .output("songbook.html")
+        .song("song.md", indoc! {r#"
+            # Song
 
-    let html = fs::read_to_string(out_dir.join("songbook.html"))
-        .unwrap()
-        .remove_newlines();
-    let re = Regex::new("<foo>.*Yippie yea.*</foo>").unwrap();
-    re.find(&html).unwrap();
+            <foo>
+
+            1. `Am`Yippie yea `C`oh!</foo>
+            <bar color="red">Yippie</bar> yea `Am`yay!
+            "#},
+        )
+        .template_prefix_default("songbook.html", "html.hbs", indoc! {r#"
+            {{#*inline "h-foo"}}<foo>{{/inline}}
+            {{#*inline "h-/foo"}}</foo>{{/inline}}
+
+            {{#*inline "h-bar"}}<span style="color:{{ color }};">{{/inline}}
+            {{#*inline "h-/bar"}}</span>{{/inline}}
+            "#},
+            &html::DEFAULT_TEMPLATE,
+        )
+        .template_prefix_default("songbook.pdf", "pdf.hbs", indoc! {r#"
+            \newenvironment{foo}{}{}
+            {{#*inline "h-foo"}}\begin{foo}{{/inline}}
+            {{#*inline "h-/foo"}}\end{foo}{{/inline}}
+
+            {{#*inline "h-bar"}}{\color{ {{~ color ~}} }{{/inline}}
+            {{#*inline "h-/bar"}}}{{/inline}}
+            "#},
+            &pdf::DEFAULT_TEMPLATE,
+        )
+        .build()
+        .unwrap();
+
+    let html = build.read_output(".html").remove_newlines();
+    html.find_re("<foo>.*Yippie yea.*</foo>").unwrap();
     html.find(r#"<span style="color:red;">Yippie</span>"#)
         .unwrap();
 
-    let tex = fs::read_to_string(out_dir.join("songbook.tex"))
-        .unwrap()
-        .remove_newlines();
-    let re = Regex::new(r"\\begin\{foo\}.*Yippie.*yea.*\\end\{foo\}").unwrap();
-    re.find(&tex).unwrap();
+    let tex = build.read_output(".tex").remove_newlines();
+    eprintln!("{}", tex);
+    tex.find_re(r"\\begin\{foo\}.*Yippie.*yea.*\\end\{foo\}")
+        .unwrap();
     tex.find(r"{\color{red}Yippie}").unwrap();
+}
+
+#[test]
+fn project_html_reserved_tags() {
+    let song = RESERVED_TAGS
+        .iter()
+        .enumerate()
+        .fold("# Song\n".to_string(), |mut s, (i, tag)| {
+            s.push_str(&format!("\n{}. <{}>text.\n", i + 1, tag));
+            s
+        });
+
+    let build = TestProject::new("html-reserved-tags")
+        .output("songbook.html")
+        .song("song.md", song)
+        .build()
+        .unwrap();
+
+    build.unwrap_err();
+    build.assert_parser_diag(DiagKind::HtmlReservedTag { tag: "html".into() });
+    build.assert_parser_diag(DiagKind::HtmlReservedTag { tag: "tex".into() });
 }
--- tests/test-projects/html/songs/yippie.md
+++
@@ -1,7 +0,0 @@
-# Yippie Yeah
-## (You can edit this content!)
-
-<foo>
-
-1. `Am`Yippie yea `C`oh!</foo>
-<bar color="red">Yippie</bar> yea `Am`yay!
